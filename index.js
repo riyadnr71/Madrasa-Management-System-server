bf8f735cba04b18def21371bc789dd76e6b79c68
Riyad Prodhan <riyadprodhan91@gmail.com>
2026-09-21 23:43:00 +0600

    Initial server setup

--- index.js
+++ index.js
@@ -60,6 +60,9 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
+      "https://schoolwebsite71.netlify.app",
+      "https://school91.netlify.app"
+
     ],
     credentials: true,
   })
